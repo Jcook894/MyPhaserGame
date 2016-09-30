1ef08a4d241ebf62b436abef164d439a0512c04e
Jcook894 <Jcook894@icloud.com>
2016-09-30 02:18:56 -0400

    Added the background img and make it scroll.

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-var game = new Phaser.Game(900, 600, Phaser.CANVAS,'gameDiv');
-
-var mainState = {
-  preload: function(){
-
-
-  },
-  create: function(){
-
-
-  },
-  update: function(){
-
-
-  }
-
-}
-
-game.state.add('mainState', mainState);
-
-game.state.start('mainState');
-
 var game = new Phaser.Game(800, 600, Phaser.CANVAS,'gameDiv');
 
+var spaceField;
+var backgroundV;
+
 var mainState = {
   preload: function(){
+    game.load.image('starField', 'assets/Starfield.png');
 
 
   },
   create: function(){
+    // Appends background to the canvas.
+    spaceField = game.add.tileSprite(0,0,800,600,'starField');
+    backgroundV = 2;
+
 
 
   },
   update: function(){
-
+    // Updates the background and makes it move up the canvas.
+    spaceField.tilePosition.y += backgroundV;
 
   }
 
